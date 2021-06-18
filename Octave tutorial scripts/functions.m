37921d24466945ecd34bab8a05ba1ce29303c9f7
Rishabh Kumar Singh <rishabhkumar812@gmail.com>
2021-06-18 16:57:20 +0530

    octave tutorial files

---
+++ Octave tutorial scripts/functions.m
@@ -0,0 +1,15 @@
+function y = squareThisNumber(x)
+
+y = x^2;
+
+% Navigate to directory:
+cd /path/to/function
+
+% Call the function:
+functionName(args)
+
+function [y1, y2] = squareandCubeThisNo(x)
+y1 = x^2
+y2 = x^3
+
+[a,b] = squareandCubeThisNo(x)
